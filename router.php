ce2ddc88dced6c0b1fd57378ba199a2829c6e58c
AugustoMilao <augusto.milao01@gmail.com>
2026-08-18 20:31:15 -0300

    router

--- router.php
+++ router.php
@@ -3,3 +3,28 @@
 //* falando sério, eu ia até usar um biblioteca ou algo do tipo, mas po, eu não começei assim, então vamos voltar as sujas origens e quem sabe mais pra..
 //* opa, primeira regra ja, cada linha com no máximo ~150 caracteres, pq? falam q facilita a leitura, vamos testar ne, continuado, mais pra frente podemos
 //* testar mudar para uma framework, ou quem sabe até de linguagem geral
+
+
+//? por enquanto vamos rodar via php msm : php -S 0.0.0.0:8000 .\router.php
+
+// foreach ($_SERVER as $atributo => $valor) {
+//     echo $atributo . '| => |';
+//     var_dump($valor);
+//     echo '<br>';
+// }
+
+$url = $_SERVER['REQUEST_URI'];
+
+$url = explode('/', $url);
+array_shift($url);
+
+
+switch ($url[0]) {
+    case 'testes':
+        echo 'eita, essa aqui existe';
+        break;
+
+    default:
+        echo 'Página não encontrada, pelo visto';
+        break;
+}
